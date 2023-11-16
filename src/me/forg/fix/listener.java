f697b8bb7d9f2b73af4f7c5d96190ebb22bf6904
Forg <frogman786@gmail.com>
2023-11-16 21:49:42 +1300

    added no drop in spawn

--- src/me/forg/fix/listener.java
+++ src/me/forg/fix/listener.java
@@ -3,8 +3,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class listener implements Listener {
 	@EventHandler (priority = EventPriority.LOWEST)
@@ -16,6 +18,16 @@ public class listener implements Listener {
 		}
 		
 
+	}
+	@EventHandler (priority = EventPriority.LOWEST)
+	public void playerdrop(PlayerDropItemEvent event) {
+		String world = event.getPlayer().getWorld().getName().toString();
+		String name = event.getPlayer().getName().toString();
+		if(world.equalsIgnoreCase("world")) {
+			event.setCancelled(true);
+			Bukkit.getLogger().info(name +" tried to drop something in spawn");
+
+		}
 	}
 	
 }
